c789555dc10a04586ba771f10b437205895237e3
Jouni Sunnari <dz4tune@gmail.com>
2020-10-14 00:50:14 +0300

    added some cities

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ from mpl_toolkits.basemap import Basemap
 
 df : pd.DataFrame = pd.read_excel('Export_GTF_IEA.XLS')
 df = df[:195]
+df : pd.DataFrame = df[(df['Exit'] != 'Liquefied Natural Gas') & (df['Entry'] != 'Liquefied Natural Gas')]
 verticies : pd.DataFrame = df[['Exit', 'Entry']]
-verticies : pd.DataFrame = verticies[(verticies['Exit'] != 'Liquefied Natural Gas') & (verticies['Entry'] != 'Liquefied Natural Gas')]
 
 G = nx.DiGraph()
 G.add_edges_from(verticies.to_numpy())
-
+G.add_nodes_from(df['Borderpoint'])
 
 class Coordinates:
+    failed = set()
 
     def __init__(self):
-        self.df: pd.DataFrame = pd.read_csv('countries.csv', sep='\t')
+        self.countries: pd.DataFrame = pd.read_csv('countries.csv', sep='\t')
+        self.cities: pd.DataFrame = pd.read_csv('worldcities.csv')
 
     def __getitem__(self, name: str) -> (float, float):
-        data_point = self.df[self.df['name'] == name]
-        if len(data_point) == 0:
-            raise KeyError(f"No such country {name}")
-        return tuple(data_point[['longitude', 'latitude']].values.reshape(2))
+        data_point = self.countries[self.countries['name'] == name]
+        if len(data_point) != 0:
+            return tuple(data_point[['longitude', 'latitude']].values.reshape(2))
+        data_point = self.cities[self.cities['city'] == name]
+        if len(data_point) != 0:
+            return tuple(data_point.iloc[0][['lng', 'lat']].values.reshape(2))
+        data_point = self.cities[self.cities['city_ascii'] == name]
+        if len(data_point) != 0:
+            return tuple(data_point.iloc[0][['lng', 'lat']].values.reshape(2))
+        # raise KeyError(f"No such country or city {name}")
+        Coordinates.failed.add(name)
+        return (0,0)
 
 
 m = Basemap()
