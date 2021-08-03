86fed4a54c46cd9a8970405778d28e863584cd03
Jouni Sunnari <dz4tune@gmail.com>
2021-08-04 01:24:42 +0300

    autoscaling

--- animator.py
+++ animator.py
@@ -4,7 +4,6 @@ class Animator:
 
     def __init__(self, mpl_proc: MplProc):
         self.mpl_proc = mpl_proc
-        self.mpl_proc.proxy_ax.set(ylim=(0, 20), xlim=(0, 10000))
 
         def init(objs):
             ax = objs['ax']
@@ -17,16 +16,47 @@ class Animator:
         self.proxy_train = self.mpl_proc.new_proxy([])
         self.proxy_test = self.mpl_proc.new_proxy([])
         self.cnt = 0
+
         self.mpl_proc.call_function(init)
-        self.proxy_ltrain = ProxyObject(self.mpl_proc.conn, 'ltrain')
-        self.proxy_ltest  = ProxyObject(self.mpl_proc.conn, 'ltest')
 
     def add(self, y1, y2):
+
+        # want this to be like that
+        # with self.mpl_proc.buffer_calls():
+            # self.proxy_train.append(y1)
+            # self.proxy_test.append(y2)
+            # rng = range(range(len(self.proxy_train)))
+            # self.proxy_ltrain.set_data(rng, self.proxy_train)
+            # self.proxy_ltest.set_data(rng, self.proxy_test)
+
         def update(objs, y1, y2):
-            objs['train'].append(y1)
-            objs['test'].append(y2)
-            rng = range(len(objs['train']))
-            objs['ltrain'].set_data(rng, objs['train'])
-            objs['ltest'].set_data(rng, objs['test'])
+
+            '''
+            wanted this to work TODO findout why doesn't work
+            def foo(train, test, ltrain, ltest, y1, y2, **_):
+
+                train.append(y1)
+                test.append(y2)
+                rng = range(len(train))
+                ltrain.set_data(rng, train)
+                ltest.set_data(rng, test)
+
+            return foo(**objs, y1=y1, y2=y2)
+            '''
+
+            train = objs['train']
+            test = objs['test']
+            ltrain = objs['ltrain']
+            ltest = objs['ltest']
+
+            train.append(y1)
+            test.append(y2)
+            rng = range(len(train))
+            ltrain.set_data(rng, train)
+            ltest.set_data(rng, test)
+
+            ax = objs['ax']
+            ax.relim()
+            ax.autoscale_view(True, True, True)
 
         self.mpl_proc.call_function(update, y1, y2)
